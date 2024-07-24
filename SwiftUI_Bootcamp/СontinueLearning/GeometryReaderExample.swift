128ab4899b6e62ed3a7b2c172ea92d37402d63af
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-07-24 14:59:10 +0100

    add horizontal rotation 3d effect

--- SwiftUI_Bootcamp/СontinueLearning/GeometryReaderExample.swift
+++ SwiftUI_Bootcamp/СontinueLearning/GeometryReaderExample.swift
@@ -9,10 +9,77 @@ import SwiftUI
 
 struct GeometryReaderExample: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //basicExample()
+        rotation3DEffectHorizontalExample()
+        //rotation3DEffectVerticalExample()
+    }
+
+    private func getHorizontalPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return 1.0 - (currentX / maxDistance)
+    }
+
+    private func getVerticalPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.height
+        let currentY = geo.frame(in: .global).minY
+        return 1.0 - ((currentY + maxDistance) / maxDistance)
+    }
+
+    private func rotation3DEffectVerticalExample() -> some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        return RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(
+                                Angle(degrees: getVerticalPercentage(geo: geometry) * 40),
+                                axis: (x: 1.0, y: 0.0, z: 0.0)
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
+        }
+    }
+
+    private func rotation3DEffectHorizontalExample() -> some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(
+                                Angle(degrees: getHorizontalPercentage(geo: geometry) * 40),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
+            }
+        }
+    }
+
+    private func basicExample() -> some View {
+        GeometryReader { geometry in
+            HStack(spacing: 0) {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: geometry.size.width * 0.6666)
+                Rectangle()
+                    .fill(Color.blue)
+            }
+            .ignoresSafeArea()
+        }
     }
 }
 
-#Preview {
+#Preview("Portrait") {
+    GeometryReaderExample()
+}
+
+#Preview("Landscape", traits: .landscapeLeft) {
     GeometryReaderExample()
 }
