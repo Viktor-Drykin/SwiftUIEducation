a0b80a4dbed0bdba288d1f5fd643c6af0b22ec3f
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-08-02 17:09:28 +0100

    add visual effect

--- SwiftUI_Bootcamp/СontinueLearning/VisualEffectExample.swift
+++ SwiftUI_Bootcamp/СontinueLearning/VisualEffectExample.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct VisualEffectExample: View {
+
+    @State private var showSpacer = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        complexExample
+        //simpleExample
+    }
+
+    private var complexExample: some View {
+        ScrollView {
+            VStack(spacing: 30) {
+                ForEach(0..<100) { index in
+                    Rectangle()
+                        .frame(width: 300, height: 300)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.orange)
+                        .visualEffect { content, geometryProxy in
+                            content.offset(x: geometryProxy.frame(in: .global).minY * 0.5)
+                        }
+                }
+            }
+        }
+    }
+
+    private var simpleExample: some View {
+        VStack {
+            Text("Hello, World! asd fas dfs adf")
+                .padding()
+                .background(Color.red)
+                .visualEffect { content, geometry in
+                    content
+                        .grayscale(geometry.frame(in: .global).minY < 300 ? 1 : 0)
+                }
+
+            if showSpacer {
+                Spacer()
+            }
+
+            Text("Hello, World!")
+                .padding()
+                .background(Color.red)
+                .visualEffect { content, geometry in
+                    content
+                        .grayscale(geometry.size.width >= 200 ? 1 : 0)
+                }
+        }
+        .background(Color.green)
+        .animation(.easeInOut, value: showSpacer)
+        .onTapGesture {
+            showSpacer.toggle()
+        }
     }
 }
 
